80c5ab76a1fe100597b44087b7505d4de43577b5
Md.Abdullah al mamun <mamun1214@gmail.com>
2024-02-17 10:10:49 +0000

    Complete challenge one

--- src/Member.php
+++ src/Member.php
@@ -1,1 +1,29 @@
 <?php
+class Member
+{
+    const ADMIN_ROLE = 'admin';
+    const DEFAULT_ROLE = 'member';
+
+    public string $username;
+    public string $role;
+
+    public function __construct()
+    {
+    }
+
+    public function addWorkspaceMember()
+    {
+    }
+
+    public function createChat()
+    {
+    }
+
+    public function createWorkspace()
+    {
+    }
+
+    public function postMessageToChat()
+    {
+    }
+}
--- src/Workspace.php
+++ src/Workspace.php
@@ -1,1 +1,9 @@
 <?php
+class Workspace
+{
+    public string $url;
+    public array $chats = [];
+    public array $members = [];
+
+    public static string $urlDomain = '.flack.app';
+}
